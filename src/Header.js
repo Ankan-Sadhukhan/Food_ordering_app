d7ec146ede95d9d99ebd422ce8f85db1d4d81124
[Ankan Sadhukhan] <[sadhukhanankan7@gmail.com]>
2023-07-05 14:02:12 +0530

    creatde folder structure

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+const Header = ()=>(
+    <div className="nav">
+        <a href="/">
+            <img className="logo" alt="logo" src="https://www.f6s.com/content-resource/profiles/2214242_original.jpg"></img>
+        </a>
+        <div className="nav-items">
+            <h4>Home</h4>
+            <h4>About us</h4>
+            <h4>contact</h4>
+            <h4>Cart</h4>
+        </div>
+
+    </div>
+);
+
+export default Header;
